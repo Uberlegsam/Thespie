7e37d598465626174df8371536ec663739af2df5
Shymaxer_ya <shymaxer.ya@gmail.com>
2020-01-16 12:35:55 +0300

    added imports

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,15 @@
 
 document.addEventListener('DOMContentLoaded', () => {
     window.refs = {
-    //   sharedLogic: new SharedLogic(),
+        //   sharedLogic: new SharedLogic(),
     };
+    let el = document.querySelectorAll('.class');
+    let length = el.length;
+  
+    for (let i = 0; i < length-1; i++) {
+        el[i].classList.add('class1');
+    }
+    el[length-1].classList.add('class2');
 })
+
+
