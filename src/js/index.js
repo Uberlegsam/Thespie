ffe2bd64906fdc9b6d7fdc8aa8158831ecd7f2ab
Uberlegsam <konstantin.kizeew@gmail.com>
2020-01-16 12:50:03 +0300

    fix

--- src/js/index.js
+++ src/js/index.js
@@ -4,13 +4,6 @@ document.addEventListener('DOMContentLoaded', () => {
     window.refs = {
         //   sharedLogic: new SharedLogic(),
     };
-    let el = document.querySelectorAll('.class');
-    let length = el.length;
-  
-    for (let i = 0; i < length-1; i++) {
-        el[i].classList.add('class1');
-    }
-    el[length-1].classList.add('class2');
 })
 
 
